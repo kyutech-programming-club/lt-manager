86159e9fc669b336395ae3633c84fcc7e995da61
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-08 20:21:28 +0900

    Add create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,28 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path
+    else
+      render new_user_path
+    end
 
   end
 
   def index
 
   end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
+
 end
