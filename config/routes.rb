f16f5262c97c65e1ab969ddc79c78901f2502fcf
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-09 11:38:26 +0900

    Add login in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  root 'users#top'
 
+  root 'users#top'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
   resources "users"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
