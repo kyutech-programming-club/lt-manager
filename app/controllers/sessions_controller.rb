aa1fb799e08aa2e860351586672ba8de29b56d91
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-09 14:12:44 +0900

    Add include SessionsHelper in application_helper.rb

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,7 +3,12 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
 
+    else
+      render 'new'
+    end
   end
 
   def destroy
