865da4511669f608d709da7e0e28b61d1c4bd258
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-09 11:34:22 +0900

    rails g controller Sessions new

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'users#top'
+
   resources "users"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
