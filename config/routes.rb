c52d4d7b00fbe74bc3a4fbae9b87e3ad5a15f44c
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-09 17:48:33 +0900

    Add logout

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'users#top'
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
-  get 'logout' => 'sessions#destroy'
+  delete 'logout' => 'sessions#destroy'
   resources "users"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
