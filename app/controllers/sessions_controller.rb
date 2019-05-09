59c4781a14228d346b0f431747727a766d640258
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-09 17:38:47 +0900

    Add login ok

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,10 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email])
-    if user && user.authenticate(params[:session][:password])
-
+    user = User.find_by(name: params[:name])
+    if user && user.authenticate(params[:password])
+      log_in user
+      redirect_to root_path
     else
       render 'new'
     end
