619b59521a8fb2fd2dc18eb5d296663bfe671e3f
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-10 20:14:45 +0900

    Add link top -> event/index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -10,4 +10,24 @@ class EventsController < ApplicationController
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to event_path(id: @event.id)
+    else
+      render new_event_path
+    end
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :start_time, :end_time, :location)
+  end
+
 end
