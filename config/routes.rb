45defc84fa2b16750848f0e726904272d3c13e7a
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-10 20:22:33 +0900

    shusei

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
 
-  root 'users#top'
-  get 'login' => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+  root "users#top"
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
   resources "users"
   resources "events"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
