6497ed783628971e18076114fac67b8c03e67e70
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-05-22 22:06:10 +0900

    kinoudeketa

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,15 @@
 class ReviewsController < ApplicationController
+
+  def create
+    talk = Talk.find(params[:talk_id])
+    review = talk.reviews.build(comment: params[:comment])
+    review.user_id = current_user.id
+    if review.save
+      #flash
+      redirect_to talk
+    else
+      redirect_to talk
+    end
+  end
+
 end
