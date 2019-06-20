0159bc4f097060db2655ff4d41eeff987be3709e
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-06-20 18:40:40 +0900

    heroku date

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_17_083740) do
+ActiveRecord::Schema.define(version: 2019_06_20_093744) do
 
   create_table "events", force: :cascade do |t|
     t.string "title"
-    t.datetime "start_time"
-    t.datetime "end_time"
     t.string "location"
     t.text "talks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "start_time"
+    t.datetime "end_time"
   end
 
   create_table "reviews", force: :cascade do |t|
