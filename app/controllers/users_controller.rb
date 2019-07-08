a1dd365d79876e0e56e9bc0c7e52a3d5e0d26873
millefeuille-shinzentaishi <nozomi.hikari.91227@gmail.com>
2019-07-08 19:35:58 +0900

    user delete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,12 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    user = User.find(params[:id])
+    user.destroy
+    redirect_to users_path
+  end
+
   private
 
   def user_params
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,7 +1,7 @@
 class Talk < ApplicationRecord
   belongs_to :user
   belongs_to :event
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 
   validates :title, presence: true
   validates :slide_url, unless: Proc.new { |a| a.slide_url.blank?}, format: /\A#{URI::regexp(%w(http https))}\z/
