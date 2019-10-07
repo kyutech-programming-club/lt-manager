4e69e152967fb0294f09e51129e23ff3e40cfbf2
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-10-07 13:14:04 +0900

    Embed movie from url

--- app/helpers/talks_helper.rb
+++ app/helpers/talks_helper.rb
@@ -1,2 +1,24 @@
 module TalksHelper
+  def modifyUrl(url){
+
+
+      return: url
+  }
+
+
+  end
+  def movie(url)
+    url["watch?v="] = "embed/"
+    url[/&.*/] = ""
+    iframe = content_tag(
+        :iframe,
+        '', # empty body
+        width: 560,
+        height: 315,
+        src: "#{url}",
+        frameborder: 0,
+        allowfullscreen: true
+    )
+    content_tag(:div, iframe, class: 'youtube-container')
+  end
 end
