aa60a0a2c471f0a46c180ddb3943ad8d4ec6a61b
tanacchi <q111026d@mail.kyutech.jp>
2020-03-19 15:05:18 +0900

    Upgrade reviews.js for realtime-review-update

--- app/assets/javascripts/reviews.js
+++ app/assets/javascripts/reviews.js
@@ -1,11 +1,20 @@
 $(function() {
+  function make_review_template(review) {
+    return `<li class="review" data-id="${review.id}">\
+              ${review.comment}\
+            </li>`;
+  }
+
+  function build_new_review(review) {
+    let reviews = $('#review-list').append(make_review_template(review));
+  }
+
   $(function() {
     setInterval(update, 1000);
   });
 
   function update() {
-    let latest_review_id = $('.review:last').data('id');
-    console.log('latest_review_id: ' + latest_review_id);
+    let latest_review_id = $('.review')[0] ? $('.review:last').data('id') : 0;
     $.ajax({
       url: location.href,
       type: 'GET',
@@ -13,6 +22,11 @@ $(function() {
         review: { id: latest_review_id }
       },
       dataType: 'json'
+    })
+    .always(function(data) {
+      $.each(data, function(i, data) {
+        build_new_review(data);
+      });
     });
   }
 });
